631a05da6ecdea56cf5746e556a5f7420c262552
Leo <Leo@DESKTOP-FCMPPV6>
2023-01-18 01:48:39 -0300

    app initialization

---
+++ src/app/components/app-tourist-destinations/app-tourist-destinations.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-tourist-destinations',
+  templateUrl: './app-tourist-destinations.component.html',
+  styleUrls: ['./app-tourist-destinations.component.scss']
+})
+export class AppTouristDestinationsComponent {
+
+}
