b8dc935ef26db0c6256c6514a1e0e2602357ccc8
Leo <Leo@DESKTOP-FCMPPV6>
2023-01-18 01:49:15 -0300

    app initialization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppToolbarComponent } from './components/app-toolbar/app-toolbar.component';
+import { AppNavbarComponent } from './components/app-navbar/app-navbar.component';
+import { AppTouristDestinationsComponent } from './components/app-tourist-destinations/app-tourist-destinations.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppToolbarComponent,
+    AppNavbarComponent,
+    AppTouristDestinationsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
